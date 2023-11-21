f9304bbf45526676147fa0704d4f7208cf38f7fd
林恩 <2475274997@qq.com>
2023-11-21 23:00:26 +0800

    修正KOOK BUG

--- bots/kook.py
+++ bots/kook.py
@@ -35,12 +35,24 @@ async def change_mode(msg: Message):
     user_chat_histories[msg.author.id]['mode'] = mode
     await msg.reply(f"已切换到 {mode} 模式。")
 
+
 # 注册消息处理器，处理所有消息
-@bot.command(regex=r'[\s\S]*')
-async def handle_message(msg: Message):
+@bot.command(regex=r'^[^/].*')
+async def process_message(msg: Message):
     print(msg.content)
     if msg.author.id not in user_chat_histories:
-        await send_intro(msg)
+        intro = "你好呀，这里是心灵加油站，如果你想要得到我的帮助的话，请你介绍下自己吧！"
+        await msg.reply(intro)
+        await msg.reply(
+            "本对话中的医学内容仅供参考，并不能视作专业意见。如需获取医疗帮助或意见，请咨询专业人士。详见医学声明.")
+        user_chat_histories[msg.author.id] = {
+            'history': [
+                {'role': 'system', 'content': INTRO_MSG},
+                {'role': 'user', 'content': INTRO_MSG}
+            ],
+            'mode': 'ChatGPT'
+        }
+        gpt_35_api_stream(user_chat_histories[msg.author.id]['history'])
 
     user_input = {
         'role': 'user',
@@ -55,7 +67,8 @@ async def handle_message(msg: Message):
             return
         gpt_response = user_chat_histories[msg.author.id]['history'][-1]['content']
 
-    await msg.reply(gpt_response)
+    # 使用 await 调用异步函数
+    msg.reply(gpt_response)
 
     if not os.path.exists("logs"):
         os.mkdir("logs")
@@ -66,6 +79,7 @@ async def handle_message(msg: Message):
     with open(file_path, "a", encoding="utf-8") as f:
         f.write(f"用户: {user_input['content']}\n")
         f.write(f"机器人: {gpt_response}\n")
+
 
 # 注册命令，发送 `/reset` 在频道中调用
 @bot.command(name='reset')
